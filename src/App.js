f0915658e27a543c8caf64c90371046a124d9f77
Sahas <sahasvivek001@gmail.com>
2022-05-29 18:58:53 +0530

    feat: initial setup + dependencies setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Signup from './pages/Student/Signup/Signup';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import Home from './pages/Student/Home/Home';
+import Classes from './pages/Student/Classes/Classes';
+import Profile from './pages/Student/Profile/Profile';
+import Videointerface from './pages/Student/Videointerface/Videointerface';
+import HomeAdmin from './pages/Admin/Home/Home';
+import Login from './pages/Student/Login/Login';
 
-function App() {
+const theme = createTheme({
+  palette: {
+    type: "dark",
+    background: {
+      default: "#081220",
+      paper: "#081220",
+    },
+    primary: {
+      main: "#1799E1",
+    },
+    secondary: {
+      main: "#1799E1",
+    },
+    // whitetext: {
+    // 	main: "#fafafa",
+    // },
+    // pureWhite: {
+    // 	main: "#FFFFFF",
+    // },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  }
+});
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/user/signup" component={Signup} />
+          <Route path='/user/home' component={Home} />
+          <Route path='/user/classes' component={Classes} />
+          <Route path='/user/profile' component={Profile} />
+          <Route path='/user/video' component={Videointerface} />
+          <Route path='/admin/home' component={HomeAdmin} />
+          <Route path='/user/login' component={Login} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
